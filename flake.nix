51aee0c31a9d41d7c7666d65140c86c5c7bf55e4
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-04-07 19:28:30 +0800

    update

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             mdbook
+            mitscheme
           ];
           shellHook = ''
             cd tutorial/
